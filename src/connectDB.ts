c5ac93523265cf02921b7cefb21f63fe4ec95159
SantiagoCamelo234 <ilvarcamelo206@gmail.com>
2025-05-28 13:49:45 -0500

    deploy5

--- src/connectDB.ts
+++ src/connectDB.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = 'mongodb://localhost:27017/miapi'
+const MONGO_URI = process.env.MONGODB_URI || ''
 
 export const connectDB = async () => {
     try {
@@ -9,7 +9,7 @@ export const connectDB = async () => {
     }
     catch(error){
         console.log('No se pudo conectar la base de datos', error)
-        throw Error
+        throw error
     }
 
 }
